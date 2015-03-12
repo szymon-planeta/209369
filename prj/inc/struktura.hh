c4f5c37d305c19f7d47c4ba5bb225ba9a5a1745e
Szymon Planeta <209369@student.pwr.edu.pl>
2015-03-12 10:05:08 +0100

    pliki

---
+++ prj/inc/struktura.hh
@@ -0,0 +1,17 @@
+//struktura.hh
+
+#ifndef STRUKTURA_HH
+#define PROGRAM_HH
+
+/*!\file
+ * \brief Definicja abstrakcyjnej klasy Struktura
+ */
+
+/*!
+ * \brief Modeluje abstrakcyjna klase Struktura
+ *
+ * Klasa Struktura zawiere zmienne oraz metody wspolne dla wszystkich 
+ * implementowanych struktur.
+ */
+class Struktura{
+};
